f4ec19be18d17cb9238d6d1cdebeb36becc559cc
Rishik Sahu <rishikkr@hotmail.com>
2023-11-04 00:15:53 +0530

    updated

--- frontend/src/pages/customer/LoginPage.js
+++ frontend/src/pages/customer/LoginPage.js
@@ -73,12 +73,12 @@ const LoginPage = () => {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link to="#" variant="body2">
+              <Link className="link-text" to="#" variant="body2">
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link to="/register" variant="body2">
+              <Link className="link-text" to="/register" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
