72138e6e27fe7ec8fe4de060b8b34a4e62cc6b54
Nakul Prasad <nakulprasad10@outlook.com>
2023-11-04 14:11:27 +0530

    added firebase

--- frontend/src/api/productApis.js
+++ frontend/src/api/productApis.js
@@ -1,16 +1,28 @@
 import axiosApi from "../utils/axiosConfig";
 
-const searchProductsByQuery = async ({query}) => {
+const searchProductsByQuery = async ({ query }) => {
 
-    try{
+    try {
         const data = await axiosApi.get(`/products/search?query=${query}`)
-        if(data){
-            console.log(data)    
+        if (data) {
+            console.log(data)
         }
-    }catch(err){
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+const addProduct = async ({ query }) => {
+
+    try {
+        const data = await axiosApi.post(`/products/add-product`)
+        if (data) {
+            console.log(data)
+        }
+    } catch (err) {
         console.log(err)
     }
 
 }
 
-export {searchProductsByQuery}
+export { searchProductsByQuery }
