ccef28df2f8f71d04ebdb15c2317c60d23bd0ffb
Pushker Prakash <pprakash.1st@gmail.com>
2023-11-04 20:30:35 +0530

    Order Routes Changes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -47,19 +47,25 @@ const orderRoutes = (db, firebaseApp) => {
   
 
   // Route for updating order status *Fix this not working*
-  router.delete("/delete-order/", (req, res) => {
-    const order_id = req.params.order_id;
-    console.log(order_id)
-    const orderRef = doc(dbs, "orders", order_id);
+  router.delete("/delete-order/:order_id", async (req, res) => {
+    const orderId = req.query.order_id;
+    try {
+        // Query for the document with the matching order_id field
+        const querySnapshot = await db.collection('orders').where('order_id', '==', orderId).get();
 
-    deleteDoc(orderRef)
-        .then(() => {
-            res.status(200).json({ message: "Order deleted successfully" });
-        })
-        .catch((error) => {
-            console.error("Error deleting the order:", error);
-            res.status(500).json({ error: "Deleting order failed" });
-        });
+        if (querySnapshot.empty) {
+            return res.status(404).json({ message: "No matching order found" });
+        }
+
+        // Assuming there's only one order with this order_id
+        const orderDoc = querySnapshot.docs[0];
+
+        await db.collection('orders').doc(orderDoc.id).delete();
+        res.status(200).json({ message: "Order deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting the order:", error);
+        res.status(500).json({ error: "Deleting order failed" });
+    }
 });
 
 
