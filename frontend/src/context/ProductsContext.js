261acdec3ebd408a166926c3cee1b37b177ca83f
rangercoder <63921044+rangercoder@users.noreply.github.com>
2023-11-04 17:06:08 +0530

    Delete frontend directory

--- frontend/src/context/ProductsContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-
-const ProductContext = createContext(null)
-
-export default ProductContext
--- frontend/src/utils/axiosConfig.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const axiosApi = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL,
-})
-
-export default axiosApi
